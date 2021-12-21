f630258f99448fe37e31b0a9437517104e558c15
das08 <takeda5955@gmail.com>
2021-12-21 22:50:09 +0900

    init

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+type User struct {
+	uid        string
+	platform   string
+	level      int
+	trio_rank  int
+	arena_rank int
+}
+
+func main() {
+	fmt.Printf("a")
+}
