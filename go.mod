405084d7470421645c08de7e6f91c9698052d7b6
das08 <takeda5955@gmail.com>
2021-12-22 21:19:01 +0900

    add sqlite

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module apexstalker-go
 
 go 1.17
 
-require github.com/joho/godotenv v1.4.0 // indirect
+require (
+	github.com/joho/godotenv v1.4.0 // indirect
+	github.com/mattn/go-sqlite3 v1.14.9 // indirect
+)
--- models/db.go
+++ models/db.go
@@ -1,9 +1,23 @@
 package models
 
+import (
+	"database/sql"
+
+	_ "github.com/mattn/go-sqlite3"
+)
+
 type User struct {
 	uid        string
 	platform   string
 	level      int
 	trio_rank  int
 	arena_rank int
+}
+
+var DbConnection *sql.DB
+
+func main() {
+	DbConnection, _ := sql.Open("sqlite3", "./apex.db")
+	defer DbConnection.Close()
+
 }
